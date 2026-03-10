3e48782d1fc4bf4f642047aea0d979ae2c0ae076
Robert <robertl87@users.noreply.github.com>
2026-03-10 22:07:07 +0000

    Update Align - Hide Backlog Rank button.user.js

--- Align - Hide Backlog Rank button.user.js
+++ Align - Hide Backlog Rank button.user.js
@@ -1,9 +1,12 @@
 // ==UserScript==
 // @name         Align - Verberg Backlog Rank knop
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Verbergt de knop "Apply Backlog Rank" zodat je de FPS priolijst niet kan vernaggelen.
 // @author       Robert
+// @updateURL    https://github.com/robertl87/userscripts/raw/refs/heads/main/Align%20-%20Hide%20Backlog%20Rank%20button.user.js
+// @downloadURL  https://github.com/robertl87/userscripts/raw/refs/heads/main/Align%20-%20Hide%20Backlog%20Rank%20button.user.js
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=jiraalign.com
 // @match        https://*.jiraalign.com/ForecastMainPage*
 // @grant        none
 // ==/UserScript==
