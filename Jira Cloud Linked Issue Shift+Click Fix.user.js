aff8692c29f2fb1abc7cc52da5274ce0588b58a4
Robert <robertl87@users.noreply.github.com>
2026-03-10 22:23:45 +0000

    Update Jira Cloud Linked Issue Shift+Click Fix.user.js

--- Jira Cloud Linked Issue Shift+Click Fix.user.js
+++ Jira Cloud Linked Issue Shift+Click Fix.user.js
@@ -2,9 +2,12 @@
 // ==UserScript==
 // @name         Jira Cloud Linked Issue Shift+Click Fix
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.2
 // @description  Open Jira linked issues in new window on Shift+Click or Ctrl+Shift+Click
-// @author       Robret
+// @updateURL    https://github.com/robertl87/userscripts/raw/refs/heads/main/Jira%20Cloud%20Linked%20Issue%20Shift+Click%20Fix.user.js
+// @downloadURL  https://github.com/robertl87/userscripts/raw/refs/heads/main/Jira%20Cloud%20Linked%20Issue%20Shift+Click%20Fix.user.js
+// @icon         https://www.pngall.com/wp-content/uploads/15/Jira-Logo-Transparent-180x180.png
+// @author       Robert
 // @match        https://*.atlassian.net/*
 // @grant        none
 // ==/UserScript==
